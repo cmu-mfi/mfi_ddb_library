5812bc2ed76783b690e6e094891c6b64a73a47c6
shobhitagg <shobhita@andrew.cmu.edu>
2025-01-11 23:45:42 -0500

    feat: add example script for streaming local files to MQTT

---
+++ examples/stream_localfiles.py
@@ -0,0 +1,18 @@
+
+import os
+
+import yaml
+
+from mfi_ddb import LocalFilesDataObject, PullStreamToMqttSpb
+
+if __name__ == "__main__": 
+    
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    mqtt_cfg = os.path.join(current_dir, 'mqtt.yaml')
+    localfiles_cfg_file = os.path.join(current_dir, 'localfiles.yaml')
+    
+    with open(localfiles_cfg_file, 'r') as file:
+        localfiles_cfg = yaml.load(file, Loader=yaml.FullLoader)
+    
+    data_obj = LocalFilesDataObject(localfiles_cfg)
+    PullStreamToMqttSpb(mqtt_cfg, data_obj)
