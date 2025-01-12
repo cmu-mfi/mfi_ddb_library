31ee09952d39904f2929ea6faebec206c9e6b1b5
shobhitagg <shobhita@andrew.cmu.edu>
2025-01-11 23:45:31 -0500

    fix: update LocalFilesDataObject

--- mfi_ddb/data_objects/local_files.py
+++ mfi_ddb/data_objects/local_files.py
@@ -1,6 +1,7 @@
 import platform
 import time
 
+import numpy as np
 import yaml
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
@@ -12,13 +13,13 @@ class LocalFilesDataObject(BaseDataObject, FileSystemEventHandler):
     def __init__(self, config, create_observers=True) -> None:
         super().__init__()
         
-        self.config = config
-        
+        self.cfg = config
         system_config = config['system']
         self.system_name = system_config['name']
+        self.system_name = f'lfs/{self.system_name}'
         
         # Initializing BaseDataObject data members
-        self.component_ids.append(f'lfs/{self.system_name}')
+        self.component_ids.append(self.system_name)
         self.data[self.system_name] = {}
         self.attributes[self.system_name] = system_config
         
@@ -34,21 +35,19 @@ class LocalFilesDataObject(BaseDataObject, FileSystemEventHandler):
                 observer.start()
                 print(f"Watching directory {dir}")
         
-    def get_data(self):
-        if len(self.buffer_data) == 0 and len(self.data[self.system_name]) == 0:
-            # Initialize a start text file with config
-            time_now = time.strftime("%Y%m%d-%H%M%S")
-            
-            for each_dir in self.config['watch_dir']:
-                with open(f"{each_dir}/mfi_ddb_start_{time_now}.txt", "w") as file:
-                    file.write(yaml.dump(self.config))
-                    
-            self.data[self.system_name]['name'] = "mfi_ddb_start_{time_now}.txt"
-            self.data[self.system_name]['timestamp'] = time_now
-            self.data[self.system_name]['file'] = bytearray(yaml.dump(self.config),'utf-8')
-            
-            return
+        # Initialize a start text file with config
+        time_now = time.strftime("%Y%m%d-%H%M%S")
         
+        for each_dir in self.cfg['watch_dir']:
+            with open(f"{each_dir}/mfi_ddb_start_{time_now}.txt", "w") as file:
+                file.write(yaml.dump(self.cfg))      
+        
+        print("Waiting for LocalFilesDataObject to initialize ...")
+        time.sleep(self.cfg["wait_before_read"]*2)
+        print("LocalFilesDataObject initialized.")
+        
+    def get_data(self): 
+    
         if len(self.buffer_data) > 0:
             data = self.buffer_data.pop(0)
             self.data[self.system_name] = data
@@ -72,20 +71,20 @@ class LocalFilesDataObject(BaseDataObject, FileSystemEventHandler):
         
         print(f"New file created: {event.src_path}")
         
-        time.sleep(self.config["wait_before_read"])
+        time.sleep(self.cfg["wait_before_read"])
         
         data = {}
-        data["name"] = self.get_event_data(event, 'name')
-        data["timestamp"] = self.get_event_data(event, 'timestamp')
-        data["file"] = self.get_event_data(event, 'file')
+        data["name"] = self.__get_event_data(event, 'name')
+        data["timestamp"] = self.__get_event_data(event, 'timestamp')
+        data["file"] = self.__get_event_data(event, 'file')
         
-        if len(self.buffer_data) < self.config["buffer_size"]:
+        if len(self.buffer_data) < self.cfg["buffer_size"]:
             self.buffer_data.append(data)
         else:
-            print(f"WARNING: Buffer full. Discarding file: {event.src_path}")
+            print(f"WARNING: Buffer full. Ignoring file: {event.src_path}")
             print("Consider increasing buffer size or stream_rate in config.")
 
-    def get_event_data(self, event, key):
+    def __get_event_data(self, event, key):
         """
         Retrieves specific data from the event based on the provided key.
         Parameters:
