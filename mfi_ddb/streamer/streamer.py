d4eec8422dc0fdf5d0279bdf8230158c24d78649
Shobhit Aggarwal <shobhita@andrew.cmu.edu>
2025-03-17 14:21:44 -0400

    streamer complete

--- mfi_ddb/streamer/streamer.py
+++ mfi_ddb/streamer/streamer.py
@@ -1,14 +1,20 @@
 import time
 
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as paho_mqtt
 from observer import Observer
 
-# TODO: make __init__.py of data_adapters so that python file need not be referenced
-# For example `from mfi_ddb.data_adapters import BaseDataAdapter` becomes valid
 from mfi_ddb.data_adapters import *
-from mfi_ddb.data_adapters.base import BaseDataAdapter
+from mfi_ddb.topic_families import *
 from mfi_ddb.utils.exceptions import ConfigError
 
+from ._mqtt import Mqtt
+from ._mqtt_spb import MqttSpb
+
+TOPIC_CLIENTS = {
+    "historian": ("MqttSpb", "HistorianTopicFamily"),
+    "kv": ("Mqtt", "KeyValueTopicFamily"),
+    "blob": ("Mqtt", "BlobTopicFamily"),
+}
 
 class Streamer(Observer):
     def __init__(self, config: dict, data_adp: BaseDataAdapter) -> None:
@@ -16,50 +22,45 @@ class Streamer(Observer):
         
         self.cfg = config
         topic_family_name = config['topic_family']
-        # TODO: spbv topic family also prefixes metric name keys with site/area if provided in mqtt config
-        self.topic_family = None
-        if 'historian' in topic_family_name:
-            self.topic_family = HistorianTopicFamily()
-        elif 'kv' in topic_family_name:
-            self.topic_family = KeyValueTopicFamily()
-        elif 'blob' in topic_family_name:
-            self.topic_family = BlobTopicFamily()
-        else:
-            raise ConfigError(f"Invalid topic family name: {topic_family_name}")
         
-        self.data_adp = data_adp
+        topic_family = globals()[TOPIC_CLIENTS[topic_family_name][1]]
+        self.__client = globals()[TOPIC_CLIENTS[topic_family_name][0]](config, topic_family)
         
-        self.connect()
+        self.__data_adp = data_adp
         
-        self.data_adp.get_data()
-        self.publish_birth()
+        self.__client.connect()
         
-        # private data members
+        self.__data_adp.get_data()
+        self.__client.publish_birth()
+        
         self.__last_poll_update = 0
         
     def reconnect(self, config=None):
         config = self.cfg if config==None else config
-        self.disconnect()
+        self.__client.disconnect()
         
         wait_time = 1
         print(f"Trying to reinitialize Data Adapter and connect in {wait_time} second...")
         time.sleep(wait_time)
         
-        data_adp = self.data_adp.__class__(self.data_adp.cfg)
+        data_adp = self.__data_adp.__class__(self.__data_adp.cfg)
         self.__init__(config, data_adp)
     
     def poll_and_stream_data(self, polling_rate_hz: int = 1):
         
-        while (time.time() - self.__last_poll_update) > 1:
+        while (time.time() - self.__last_poll_update) < 1/polling_rate_hz:
             time.sleep(0.1)            
         
-        self.data_adp.get_data()
-        self.stream_data(self.data_adp.data)
-        self.clear_data_buffer()
+        self.__data_adp.get_data()
+        self.__client.stream_data(self.__data_adp.data)
+        self.__data_adp.clear_data_buffer()
                 
         self.__last_poll_update = time.time()
         
-        
     def on_data_update(self, data: dict):
-        self.stream_data(data)
-        self.clear_data_buffer(list(data.keys()))
+        self.__client.stream_data(data)
+        self.__data_adp.clear_data_buffer(list(data.keys()))
+
+    def stream_data(self):
+        self.__client.stream_data(self.__data_adp.data)
+        self.__data_adp.clear_data_buffer()
