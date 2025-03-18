4aff404839471b892e69f954f1d6c170a14020c9
Shobhit Aggarwal <shobhita@andrew.cmu.edu>
2025-03-18 16:42:42 -0400

    refactor minor changes. ROS done

--- mfi_ddb/data_adapters/ros.py
+++ mfi_ddb/data_adapters/ros.py
@@ -6,6 +6,7 @@ from mfi_ddb.data_adapters.base import BaseDataAdapter
 
 MAX_ARRAY_SIZE = 16
 
+
 class RosDataAdapter(BaseDataAdapter):
     def __init__(self, config: dict) -> None:
         super().__init__()
@@ -27,21 +28,21 @@ class RosDataAdapter(BaseDataAdapter):
             raise Exception(
                 "ROS1 libraries not found. Please install ROS1 libraries to use RosDataAdapter."
             )
-            
+
         # CHECK CONFIG FOR REQUIRED KEYS
         if "devices" not in config.keys():
             raise Exception("No devices found in the config file.")
-        
+
         if "max_wait_per_topic" not in config.keys():
             config["max_wait_per_topic"] = 1
-            
+
         if "set_ros_callback" not in config.keys():
             config["set_ros_callback"] = False
 
         # INIT DATA MEMBERS FROM CONFIG
         self.cfg = config
         self.raw_data = {}
-        self.raw_data_filter = {}
+        self.byte_data_filter = {}
         for device in self.cfg["devices"]:
             print(f"Device: {device}")
             cfg = self.cfg["devices"][device]
@@ -49,15 +50,14 @@ class RosDataAdapter(BaseDataAdapter):
             self.component_ids.append(namespace)
 
             self.attributes[namespace] = cfg["attributes"]
-            if "experiment_class" not in cfg["attributes"].keys():
-                if "experiment_class" in self.cfg.keys():
-                    self.attributes[namespace]["experiment_class"] = self.cfg[
-                        "experiment_class"
-                    ]
+            if "trial_id" not in cfg["attributes"].keys():
+                if "trial_id" in self.cfg.keys():
+                    self.attributes[namespace]["trial_id"] = self.cfg["trial_id"]
 
-            self.data[namespace] = {}
+            self._data[namespace] = {}
+            self.last_updated[namespace] = 0
             self.raw_data[namespace] = {}
-            self.raw_data_filter[namespace] = {}
+            self.byte_data_filter[namespace] = {}
             for topic in cfg["rostopics"]:
                 if namespace != "/":
                     topic = f"/{namespace}/{topic}"
@@ -67,7 +67,7 @@ class RosDataAdapter(BaseDataAdapter):
 
                 # Data filter for each topic to avoid uint8[] array data
                 # str for status of filter, list(str) for filter
-                self.raw_data_filter[namespace][topic] = "check_needed"
+                self.byte_data_filter[namespace][topic] = "check_needed"
 
         # CHECK IF ROS MASTER IS RUNNING
         # REF: https://github.com/ros/ros_comm/blob/8250c7d434ea34d0589eb8b6eaab5df1b11fd325/tools/rostopic/src/rostopic/__init__.py#L84
@@ -77,13 +77,12 @@ class RosDataAdapter(BaseDataAdapter):
             raise Exception(
                 "ROS master is not running. Can't initialize RosDataAdapter."
             )
-        
+
         # INIT ROS NODE IF NOT ALREADY INITIALIZED
         if rospy.get_name() == "/unnamed":
             rospy.init_node("mfi_ddb_ros_adapter", anonymous=True)
-        rospy.on_shutdown(self.__ros_shutdown)            
-        
-            
+        rospy.on_shutdown(self.__ros_shutdown)
+
         # CHECK IF LISTED ROS TOPICS EXISTS
         for device in self.component_ids:
             for topic in self.raw_data[device]:
@@ -99,7 +98,7 @@ class RosDataAdapter(BaseDataAdapter):
 
                     del self.raw_data[device][topic]
                     continue
-        
+
         # SET ROS CALLBACK
         if self.cfg["set_ros_callback"]:
             self.__init_callback()
@@ -114,7 +113,7 @@ class RosDataAdapter(BaseDataAdapter):
         for device in self.component_ids:
             for topic in self.raw_data[device]:
                 self.cb_data = self.__process_rawdata(device, topic)
-                
+
     def __poll_ros_topics(self):
 
         if self.rospy.is_shutdown():
@@ -142,10 +141,10 @@ class RosDataAdapter(BaseDataAdapter):
         * filter out the variables that are not needed using .__slots__.remove(<variable_name>)
         """
         if msg is not None:
-            if self.raw_data_filter[device][topic] == "check_needed":
-                self.__check_data_filter(device, topic, msg)
+            if self.byte_data_filter[device][topic] == "check_needed":
+                self.__check_byte_data(device, topic, msg)
 
-            for key in self.raw_data_filter[device][topic]:
+            for key in self.byte_data_filter[device][topic]:
                 if key in msg.__slots__:
                     msg.__slots__.remove(key)
 
@@ -153,10 +152,10 @@ class RosDataAdapter(BaseDataAdapter):
         topic_name = topic.replace(f"/{device}/", "")
         print(f"Msg: {msg_dict}")
         new_data = self.__get_keyvalue_from_dict(msg_dict, f"{topic_name}/")
-        
-        self.data[device].update(new_data)
+
+        self._data[device].update(new_data)
         self.last_updated[device] = time.time()
-        
+
         return {device: new_data}
 
     def __get_keyvalue_from_dict(self, data_dict, key_prefix=""):
@@ -184,11 +183,11 @@ class RosDataAdapter(BaseDataAdapter):
 
         return data
 
-    def __check_data_filter(self, device, topic, msg):
+    def __check_byte_data(self, device, topic, msg):
         """
         Check if the message contains uint8[] array data
         """
-        self.raw_data_filter[device][topic] = []
+        self.byte_data_filter[device][topic] = []
 
         if "uint8[]" in msg._get_types():
             for key in msg.__slots__:
@@ -196,10 +195,9 @@ class RosDataAdapter(BaseDataAdapter):
                     msg._slot_types[msg.__slots__.index(key)] == "uint8[]"
                     and len(msg.__getattribute__(key)) > MAX_ARRAY_SIZE
                 ):
-                    self.raw_data_filter[device][topic].append(key)
+                    self.byte_data_filter[device][topic].append(key)
 
-
-    def __init_callback(self, push_mqtt_stream):
+    def __init_callback(self):
 
         # CREATE A CALLBACK FOR ALL ROSTOPICS
         if len(self.component_ids) == 0:
@@ -212,7 +210,7 @@ class RosDataAdapter(BaseDataAdapter):
                     callback=self.__callback,
                     callback_args=(device, topic),
                 )
-        
+
         self.rospy.spin()
 
     def __callback(self, anymsg, callback_args):
