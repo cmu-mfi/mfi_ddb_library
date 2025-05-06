35c81520895bfde1404286401855512563c6d6db
shobhitagg <shobhita@andrew.cmu.edu>
2025-05-06 14:27:23 -0400

    working example of CFS connector

--- examples/store_files_cfs.py
+++ examples/store_files_cfs.py
@@ -14,32 +14,41 @@ def read_config():
     return mqtt, cfs
 
 def callback(config, message):
-    data = BlobTopicFamily.process_message(message)
-    print(f"Received message with keys: {data.keys()}")
+    data_type, data = BlobTopicFamily.process_message(message)
+    print(f"Received {data_type} message with keys: {data.keys()}")
 
-    # SAVE DATA FILES
-    expected_keys = ["file_name", "file_type", "size", "timestamp", "file"]
-    if not all(key in data for key in expected_keys):
-        print("WARNING: Missing keys in the received message.")
-        return
-    
-    save_dir = config['save_directory']
-    os.makedirs(save_dir, exist_ok=True)
-    
-    file_name = f"{data["file_name"]}.{data["file_type"]}"
-    file_path = os.path.join(save_dir, file_name)
-    with open(file_path, 'wb') as file:
-        file.write(data["file"])
-    print(f"File saved at: {file_path}")
-    
-    # SAVE METADATA FILE
-    data.pop("file")
-    metadata_file_name = f"{data['file_name']}.json"
-    metadata_file_path = os.path.join(save_dir, metadata_file_name)
-    with open(metadata_file_path, 'w') as file:
-        json.dump(data, file)
-    print(f"Metadata saved at: {metadata_file_path}")
-    print("===========================\n")
+    if data_type == "attributes":
+        attributes_file_name = f"{data['trial_id']}_{data['timestamp']}.json"
+        attributes_file_path = os.path.join(config['save_directory'], attributes_file_name)
+        with open(attributes_file_path, 'w') as file:
+            json.dump(data['description'], file, indent=4)
+        print(f"Attributes saved at: {attributes_file_path}")
+        print("===========================\n")
+
+    if data_type == "data":
+        # SAVE DATA FILES
+        expected_keys = ["file_name", "file_type", "size", "timestamp", "file"]
+        if not all(key in data for key in expected_keys):
+            print("WARNING: Missing keys in the received message.")
+            return
+        
+        save_dir = config['save_directory']
+        os.makedirs(save_dir, exist_ok=True)
+        
+        file_name = f"{data['file_name']}"
+        file_path = os.path.join(save_dir, file_name)
+        with open(file_path, 'wb') as file:
+            file.write(data["file"])
+        print(f"File saved at: {file_path}")
+        
+        # SAVE METADATA FILE
+        data.pop("file")
+        metadata_file_name = f"{data['file_name']}.json"
+        metadata_file_path = os.path.join(save_dir, metadata_file_name)
+        with open(metadata_file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+        print(f"Metadata saved at: {metadata_file_path}")
+        print("===========================\n")
     
 
 if __name__ == '__main__':
@@ -48,11 +57,11 @@ if __name__ == '__main__':
     mqtt_config, cfs_config = read_config()
 
     # INIT A CONNECTION
-    mqtt_pub = Subscriber(mqtt_config, cfs_config)
+    mqtt_sub = Subscriber(mqtt_config)
 
     # SUBSCRIBE TO TOPIC AND SET A CALLBACK
     topic = get_topic_from_config(cfs_config['topic'])
-    mqtt_pub.create_callback(
+    mqtt_sub.create_message_callback(
         topic, lambda message: callback(cfs_config, message))
 
-    mqtt_pub.client.loop_forever()
+    mqtt_sub.client.loop_forever()
