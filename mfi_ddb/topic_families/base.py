e13fb2026c7762706e28ff0ea9568f4734a47cac
shobhitagg <shobhita@andrew.cmu.edu>
2025-05-06 14:38:36 -0400

    refactor process_message to a static method in base class

--- mfi_ddb/topic_families/base.py
+++ mfi_ddb/topic_families/base.py
@@ -9,5 +9,6 @@ class BaseTopicFamily:
     def process_data(self, data):
         return data
     
-    def process_message(self, message):
-        NotImplementedError(f"{self.__class__} doesn't have process_message() implemented.")
+    @staticmethod
+    def process_message(message):
+        NotImplementedError(f"process_message() not implemented.")
