46385b48b17ea705fa7d24c5e711231f141f5fbc
shobhitagg <shobhita@andrew.cmu.edu>
2025-05-06 11:56:50 -0400

    reverting changes to process_attr and process_data

--- mfi_ddb/topic_families/blob.py
+++ mfi_ddb/topic_families/blob.py
@@ -16,49 +16,44 @@ class BlobTopicFamily(BaseTopicFamily):
         if self.__trial_id is None:
             print("WARNING: trial_id is not provided in attributes")
         
-        payload = self.process_data(attributes, is_attribute=True)
+        payload = self.process_data(attributes)
         
-        # return {'attributes': payload['data']}
-        return payload
+        return {'attributes': payload['data']}
     
-    def process_data(self, data, is_attribute: bool = False):
+    def process_data(self, data):
         # Using trial_id from data if available, otherwise using last record of trial_id
         if not bool(data):
             print("WARNING: blob data is empty")
             return {}
             
+        self.__trial_id = data.get("trial_id", self.__trial_id)        
+        
         payload = blob_pb2.Payload()
         payload.timestamp = int(time.time())
-
-        for key, value in data.items():
-            self.__trial_id = value.get("trial_id", self.__trial_id)        
-            payload.trial_id = self.__trial_id
-            
-            metric = blob_pb2.Payload.Metric()
-            
-            metric.name = "data" if not is_attribute else "attributes"
-            metric.metadata.file_name = value.get("file_name", "")
-            metric.metadata.file_type = value.get("file_type", "")
-            metric.metadata.size = value.get("size", 0)
-            metric.timestamp = value.get("timestamp", payload.timestamp)
-            metric.bytes_value = value.get("file", b'')
-
-            # Put all other data in Metadata.description
-            other_metadata = {}
-            for key, value in value.items():
-                if key not in ["file_name", "file_type", "size", "timestamp", "file"]:
-                    other_metadata[key] = value
-            other_metadata['system_name'] = key
-                    
-            metric.metadata.description = str(other_metadata)
-            
-            if metric.bytes_value == b'':
-                print(f"WARNING: file is not provided in the blob data. Streaming empty file.")
-            
-            payload.metrics.append(metric)
+        payload.trial_id = self.__trial_id
         
-        # return({"data": payload.SerializeToString()})
-        return payload.SerializeToString()
+        metric = blob_pb2.Payload.Metric()
+        
+        metric.metadata.file_name = data.get("file_name", "")
+        metric.metadata.file_type = data.get("file_type", "")
+        metric.metadata.size = data.get("size", 0)
+        metric.timestamp = data.get("timestamp", payload.timestamp)
+        metric.bytes_value = data.get("file", b'')
+
+        # Put all other data in Metadata.description
+        other_metadata = {}
+        for key, value in data.items():
+            if key not in ["file_name", "file_type", "size", "timestamp", "file"]:
+                other_metadata[key] = value
+                
+        metric.metadata.description = str(other_metadata)
+        
+        if metric.bytes_value == b'':
+            print(f"WARNING: file is not provided in the blob data. Streaming empty file.")
+        
+        payload.metrics.append(metric)
+        
+        return({"data": payload.SerializeToString()})
     
     def process_message(self, message):        
         # Unpack the message
