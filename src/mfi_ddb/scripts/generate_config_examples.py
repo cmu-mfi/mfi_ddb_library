bcbfc2302d3f14a61ec2428ee266d5d2e5db8ccc
shobhitagg <shobhita@andrew.cmu.edu>
2025-10-20 18:11:49 -0400

    added streamer config schema and example

--- src/mfi_ddb/scripts/generate_config_examples.py
+++ src/mfi_ddb/scripts/generate_config_examples.py
@@ -45,8 +45,11 @@ if __name__ == "__main__":
     args.output_dir = os.path.abspath(args.output_dir)
     os.makedirs(args.output_dir, exist_ok=True)
 
-    # GENERATE CONFIGURATION EXAMPLE FILES
+    # GENERATE CONFIGURATION EXAMPLE FILES FOR DATA ADAPTERS
     # =======================================================
+
+    data_adapter_config_dir = os.path.join(args.output_dir, "data_adapters")
+    os.makedirs(data_adapter_config_dir, exist_ok=True)
 
     for adapter in data_adapters.values():
         config_example = adapter.CONFIG_EXAMPLE
@@ -55,7 +58,23 @@ if __name__ == "__main__":
         config_example_pretty = yaml.dump(config_example, sort_keys=False)
         config_help_pretty = yaml.dump(config_help, sort_keys=False)        
         
-        example_output_path = f"{args.output_dir}/{adapter.NAME.lower().replace(' ', '_')}_config.yaml"
-        help_output_path = f"{args.output_dir}/{adapter.NAME.lower().replace(' ', '_')}_help.yaml"
+        example_output_path = f"{data_adapter_config_dir}/{adapter.NAME.lower().replace(' ', '_')}_config.yaml"
+        help_output_path = f"{data_adapter_config_dir}/{adapter.NAME.lower().replace(' ', '_')}_help.yaml"
         yaml.dump(config_example, open(example_output_path, "w"), sort_keys=False)
         yaml.dump(config_help, open(help_output_path, "w"), sort_keys=False)
+        
+    # GENERATE OVERALL CONFIGURATION EXAMPLE FILE FOR STREAMER
+    # =======================================================
+    streamer_config_dir = os.path.join(args.output_dir, "streamer")
+    os.makedirs(streamer_config_dir, exist_ok=True)
+    
+    config_example = mfi_ddb.streamer.Streamer.CONFIG_EXAMPLE
+    config_help = mfi_ddb.streamer.Streamer.CONFIG_HELP
+
+    config_example_pretty = yaml.dump(config_example, sort_keys=False)
+    config_help_pretty = yaml.dump(config_help, sort_keys=False)
+
+    example_output_path = f"{streamer_config_dir}/streamer_config.yaml"
+    help_output_path = f"{streamer_config_dir}/streamer_help.yaml"
+    yaml.dump(config_example, open(example_output_path, "w"), sort_keys=False)
+    yaml.dump(config_help, open(help_output_path, "w"), sort_keys=False)
