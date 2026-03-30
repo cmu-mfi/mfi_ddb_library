77295a37e93ae656918eee315965dec4f9793809
ssterenruta <ssterenruta@cmu.edu>
2026-03-30 15:53:26 -0400

    had originally changed the name of the folder, now returned it back

--- src/mfi_ddb/databases/aveva-pi/piwebapi.py
+++ src/mfi_ddb/databases/aveva-pi/piwebapi.py
@@ -9,18 +9,8 @@ import error_codes
 class PIWebAPI:
     def __init__(self, secrets: dict):        
         self.url = secrets['url']
-        
-        # TODO: AUTHENTICATION......................................................
-        # self.auth = HTTPBasicAuth(secrets['username'], secrets['password'])
-        # https://docs.aveva.com/bundle/pi-web-api/page/1023024.html
-        # https://docs.aveva.com/bundle/pi-web-api/page/1023022.html
-        # Authentication methods:
-        # * Basic
-        # * Kerberos (Uses windows auth. Enabled by default.)
-        # * Bearer (token based. Need to install AIM and get a token from there)
-        # * Anonymous (Best. But I think works only with windows only??)      
-        # ...<insert authentication code here>...  
-        
+        self.session = requests.Session()
+        self.session.auth = HTTPBasicAuth(secrets['username'], secrets['password'])
         self.logger = logging.getLogger(__name__)
         self.__secrets = secrets
     
@@ -46,13 +36,17 @@ class PIWebAPI:
         
         #1. PARSE THE INPUTS
         webid = self.__get_topic_webid(topic)
-        iso_start_time = parse_date(start_time).astimezone(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
-        iso_end_time = parse_date(end_time).astimezone(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
-        iso_page_token = parse_date(page_token).astimezone(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ") if page_token else None
+        # iso_start_time = parse_date(start_time).astimezone(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+        # iso_end_time = parse_date(end_time).astimezone(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+        # iso_page_token = parse_date(page_token).astimezone(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ") if page_token else None
+        iso_start_time = parse_date(start_time).astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
+        iso_end_time = parse_date(end_time).astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
+        #Due to python truncating of milliseconds, this was updated to keep original values
+        iso_page_token = page_token if page_token else None
         
         if iso_page_token:
             iso_start_time = iso_page_token
-            
+
         if iso_start_time >= iso_end_time:
             raise error_codes.InvalidArgumentError("start_time must be less than end_time")
         
@@ -62,16 +56,19 @@ class PIWebAPI:
         
         # 2. CHECK THE DATA POINT COUNT FOR THE GIVEN RANGE
         get_request = f"{self.url}/streams/{webid}/summary"
+
         parameters = {
             "startTime": iso_start_time,
             "endTime": iso_end_time,
             "summaryType": "Count",
         }
+
         get_request += "?" + "&".join([f"{key}={value}" for key, value in parameters.items()])
         response = self.__make_get_request(get_request)
-        total_count = response['Items'][0]['Value'] if response['Items'] else 0
-        request_count = min(page_size, total_count)
-        
+
+        total_count = response['Items'][0]['Value']['Value'] if response['Items'] else 0
+        request_count = int(min(page_size, total_count))
+
         if request_count == 0:
             return {
                 "data": [],
@@ -85,15 +82,20 @@ class PIWebAPI:
         parameters["selectedFields"] = "Items.Timestamp;Items.Value"
         get_request += "?" + "&".join([f"{key}={value}" for key, value in parameters.items()])
         response = self.__make_get_request(get_request)
-        
-        last_timestamp = response['Items'][-1]['Timestamp']
+        items = response.get('Items', [])
+
+        #checking if the item was already sent in the previous page comparing timestamps, to avoid duplicate data sent
+        if iso_page_token and items and items[0]['Timestamp'] == iso_page_token:
+            items = items[1:]
+
+        last_timestamp = items[-1]['Timestamp'] if items else None
         data = []
-        for item in response.get('Items', []):
+        for item in items:
             data.append(item["Value"])
-            
+        
         return {
             "data": data,
-            "nextPageToken": last_timestamp if total_count > request_count else None
+            "nextPageToken": last_timestamp if items and last_timestamp != page_token else None
         }
         
         
@@ -102,12 +104,13 @@ class PIWebAPI:
     
     def __get_topic_webid(self, topic):
         get_request = f"{self.url}/points"
+        
         parameters = {
-            "path": f"\\\\{self.__secrets['dataserver']['name']}\\{self.__secrets['dataserver']['name']}/{topic}",
+            "path": f"\\\\{self.__secrets['dataserver']['name']}\\{self.__secrets['mqtt_connector']['path']}/{topic}",
             "selectedFields": "WebId"
         }
         get_request += "?" + "&".join([f"{key}={value}" for key, value in parameters.items()])
-        
+
         # example:
         # https://fms-vision.fms.local.cmu.edu/piwebapi/points?path=\\pgh-acdpi-01\MQTT_Connector.pgh-mfimqtt-01/mfi-v1.0-historian/Mill-19-test/Mezzanine-Lab/yk_destroyer/DATA/tool_offset/data/data.0
         response = self.__make_get_request(get_request)
@@ -120,9 +123,7 @@ class PIWebAPI:
         
     def __make_get_request(self, url):
         
-        # TODO: AUTHENTICATION......................................................
-        # Add authentication to the request if needed.
-        response = requests.get(url)
+        response = self.session.get(url)
         
         if response.status_code != 200:
             self.logger.error(f"GET request failed. URL: {url}, Status Code: {response.status_code}, Response: {response.text}")
